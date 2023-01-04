0dce5f51be71b235a35a3754df9d78c9eb27c15f
Nikita Ilin <ilinWall@yandex.ru>
2023-01-05 01:22:23 +0300

    update: settings

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ilinniks.pythonanywhere.com", "127.0.0.1"]
 
